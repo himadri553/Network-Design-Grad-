34e216cd653a848d79fcd7efde5906826238963c
himadri553 <himadri553@gmail.com>
2026-05-24 12:25:02 -0400

    Started project 1

---
+++ project_folders/project_1/src/main_x.py
@@ -0,0 +1,18 @@
+"""
+    Himadri Saha
+
+    main_x.py
+        - Main entry to this project, run this file to run the project
+        - Prompts user to select secnario
+
+"""
+
+""" Imports """
+import threading
+
+""" Main function """
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
