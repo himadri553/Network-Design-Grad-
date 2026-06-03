e32f866de9a6c67e78413304d6905726da35aa2f
Himadri Saha <Himadri_Saha@student.uml.edu>
2026-06-02 22:44:18 -0400

    Docs update

---
+++ project_folders/phase_2/docs/DESIGN_DOC_2.md
@@ -0,0 +1,7 @@
+## Overview of Code Architecture 
+- Sender
+- Receiver
+- Logger and plot log messages
+- Packet structure
+
+## Logic Flow During Transmission  
--- project_folders/phase_2/src/main2.py
+++ project_folders/phase_2/src/main2.py
@@ -4,10 +4,14 @@
     main_2.py
         - Main entry to this project, run this file to run the project
         - Chunk refers to picture data broken down into size main2_helper.packet_size
-        - Packet is the data sent from the sender with all the headers and stuff
+        - Packet is the data sent from the sender with all the headers
 
     TODO:
-        - Fix issue with image reconstruction from RX side (stuck!)
+        - Implement rest of RDT 2.2 logic
+            - 
+            - ACK
+        - Add things to the log - plot log messages
+        - Create plots based on time stamp
 
 """
 
@@ -27,20 +31,22 @@ def main():
         pass
     main2_helper.main_log_print("Clearing all output logs")
 
-    # Create sender and receiver threads
+    # Run Scenario 1 - No loss/bit-errors
     tx = SENDER_2()
     rx = RECEIVER_2()
     tx_thread_sc1 = threading.Thread(target=tx.run_tx_sc1)
     rx_thread_sc1 = threading.Thread(target=rx.run_rx_sc1)
-
-    # Start running receiver THEN sender, waiting for both to complete. For all scenarios
-    main2_helper.main_log_print("Starting tx and rx threads for Phase 2 - Scenario 1")
+    main2_helper.main_log_print("Phase 2: Starting Scenario 1 - No loss/bit-errors")
     rx_thread_sc1.start()
     time.sleep(0.5)
     tx_thread_sc1.start()
     rx_thread_sc1.join()
     tx_thread_sc1.join()
     main2_helper.main_log_print("Both threads complete")
+
+    # Run Scenario 2 - ACK packet bit-error
+
+    # Run Scenario 3 - Data packet bit-error
 
 if __name__ == "__main__":
     main()
